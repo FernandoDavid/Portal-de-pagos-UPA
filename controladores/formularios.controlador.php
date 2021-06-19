cdfac60ebb51deca08bf7a21761e4bd5836e40ef
Carlos Martínez <charley201050@hotmail.com>
2021-06-19 12:04:36 -0500

    User register

--- controladores/formularios.controlador.php
+++ controladores/formularios.controlador.php
@@ -6,7 +6,45 @@ class ControladorFormularios
     public static function ctrRegistro()
     {
         if (isset($_POST["curso"])) {
-            
+            if (preg_match('/^[a-zA-ZñÑáéíóúÁÉÍÓÚ 0-9]+$/', $_POST["nombre"])) {
+                $tabla = "Inscritos";
+
+                $datos = array(
+                    "nombre" => $_POST["nombre"],
+                    "telefono" => $_POST["telefono"],
+                    "direc" => $_POST["domicilio"],
+                    "correo" => $_POST["correo"],
+                    "curp" => $_POST["curp"],
+                    "rfc" => $_POST["rfc"],
+                    "sexo" => $_POST["sexoRadio"],
+                    "est_civil" => $_POST["estadoRadio"],
+                    "idCurso" => $_POST["curso"]
+                );
+                $respuesta = ModeloFormularios::mdlCrearRegistro($tabla,array_keys($datos),$datos);
+                if ($respuesta == "ok") {
+                    echo '<script>
+                    if(window.history.replaceState){
+                        window.history.replaceState(null,null,window.location.href);
+                    } 
+                    alert("Registro guardado correctamente");
+                    location.reload();
+                    </script>';
+                } else {
+                    echo '<script>
+                    if(window.history.replaceState){
+                        window.history.replaceState(null,null,window.location.href);
+                    } 
+                    alert("Error al realizar registro");
+                    </script>';
+                }
+            } else {
+                echo '<script>
+                    if(window.history.replaceState){
+                        window.history.replaceState(null,null,window.location.href);
+                    } 
+                    alert("Error al realizar registro");
+                    </script>';
+            }
         }
     }
 
--- modelos/formularios.modelo.php
+++ modelos/formularios.modelo.php
@@ -4,8 +4,42 @@ require_once "conexion.php";
 
 class ModeloFormularios
 {
-    static public function mdlCrearRegistro($tabla, $datos)
+    static public function mdlCrearRegistro($tabla, $campos, $datos)
     {
-        
+        $strCampos = '`' . implode('`,`', $campos) . '`';
+        $strValues = ':' . implode(', :', $campos);
+        $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla($strCampos) VALUES ($strValues)");
+        $stmt->bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR);
+        $stmt->bindParam(":correo", $datos["correo"], PDO::PARAM_STR);
+        $stmt->bindParam(":telefono", $datos["telefono"], PDO::PARAM_STR);
+        $stmt->bindParam(":direc", $datos["direc"], PDO::PARAM_STR);
+        $stmt->bindParam(":curp", $datos["curp"], PDO::PARAM_STR);
+        $stmt->bindParam(":rfc", $datos["rfc"], PDO::PARAM_STR);
+        $stmt->bindParam(":sexo", $datos["sexo"], PDO::PARAM_STR);
+        $stmt->bindParam(":est_civil", $datos["est_civil"], PDO::PARAM_STR);
+        $stmt->bindParam(":idCurso", $datos["idCurso"], PDO::PARAM_INT);
+        // $i = 0;
+        // foreach ($datos as $dato) {
+        //     echo '<pre>';
+        //     var_dump($dato);
+        //     var_dump($i);
+        //     var_dump(":".$campos[$i]);
+            
+        //     if (is_numeric($dato)) {
+        //         $stmt->bindParam(":" . $campos[$i], $dato, PDO::PARAM_INT);
+        //         echo 'int';
+        //     } else {
+        //         $stmt->bindParam(":" . $campos[$i], $dato, PDO::PARAM_STR);
+        //         echo 'str';
+        //     }
+        //     echo '</pre>';
+        //     $i++;
+        // }
+        // echo strval($stmt);
+        if($stmt -> execute()){
+            return "ok";
+        }else{
+            print_r(Conexion::conectar()->errorInfo());
+        }     
     }
 }
